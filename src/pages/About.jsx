e265317ec203bc00ed553a8b61684fc2fc20a6c7
bhuwan1301 <bhuwancp1301@gmail.com>
2024-10-02 13:51:58 +0530

    changed photos on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ import image1 from "@/assets/images/1.jpg";
 import image2 from "@/assets/images/2.jpg";
 import image3 from "@/assets/images/3.jpg";
 import image4 from "@/assets/images/4.jpg";
-import image5 from "@/assets/images/5.jpg";
+// import image5 from "@/assets/images/5.jpg";
 import image6 from "@/assets/images/6.jpg";
 import image7 from "@/assets/images/7.jpg";
 import { motion } from "framer-motion"
@@ -15,7 +15,13 @@ import { useState } from 'react';
 
 const About = () => {
   const images = [
-    image1, image2, image3, image4, image5, image6, image7,
+    image1, 
+    image2, 
+    image3, 
+    image4, 
+    // image5, 
+    // image6, 
+    // image7,
   ];
 
   const [direction, setDirection] = useState(-100)
